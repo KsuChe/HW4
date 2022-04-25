a06fddeb37247e24a1667dd2374c452fc4c7a2aa
KsuChe <nerro@mail.ru>
2022-04-25 22:32:06 +0300

    task 3

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        //task 2
+        //task 3
 
-        int fri = 1;
-        while (fri <= 31) {
-            System.out.println("Сегодня пятница, " + fri + " Необходимо подготовить отчет.");
-            fri = fri + 7;
+        int year = 0;
+        int start = 1822;
+        int finish = 2100;
+        for (year = 0; year < 2100; year = year + 79) {
+            if (year > start && year < finish) {
+                System.out.println(year);
+            }
         }
-    }
 
+    }
 }
