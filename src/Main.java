9c7ca6f45aec379c3df5710158484f50a6e9d3bf
KsuChe <nerro@mail.ru>
2022-04-25 21:31:02 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-
+        //task 1
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        int n = 10;
+        for (n = 10; n > 0; n--) {
+            System.out.print(n + " ");
+        }
     }
+
 }
