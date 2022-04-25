bd3d86bc1ea261d947493372b2b847c5c69ed78f
KsuChe <nerro@mail.ru>
2022-04-25 21:58:34 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        //task 1
-        int i = 0;
-        while (i < 10) {
-            i++;
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        int n = 10;
-        for (n = 10; n > 0; n--) {
-            System.out.print(n + " ");
+        //task 2
+        int fri = 1;
+        while (fri <= 31) {
+            System.out.println("Сегодня пятница, " + fri + " Необходимо подготовить отчет.");
+            fri = fri + 7;
         }
     }
 
